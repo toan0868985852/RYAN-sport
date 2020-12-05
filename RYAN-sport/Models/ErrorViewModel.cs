0795774ccf026a455b34df98fcefec63be509f37
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-12-05 15:41:41 +0700

    Add project files.

---
+++ RYAN-sport/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RYAN_sport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
