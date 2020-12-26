5e8aac6ea32d52069da327b23ea238d054d79163
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-12-26 17:16:48 +0700

    admin

---
+++ Admin/Admin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Admin.Areas.Identity.Data;
+using Admin.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Admin.Areas.Identity.IdentityHostingStartup))]
+namespace Admin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AdminContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AdminContextConnection")));
+
+                services.AddDefaultIdentity<AdminUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AdminContext>();
+            });
+        }
+    }
+}
