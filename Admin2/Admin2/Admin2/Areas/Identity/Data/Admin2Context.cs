8b6e35ee7ae844afebd3ebcb07e5de271c9f78f6
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-12-29 16:16:06 +0700

    login addmin

---
+++ Admin2/Admin2/Admin2/Areas/Identity/Data/Admin2Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Admin2.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin2.Data
+{
+    public class Admin2Context : IdentityDbContext<Admin2User>
+    {
+        public Admin2Context(DbContextOptions<Admin2Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Admin2/Admin2/Admin2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Admin2.Areas.Identity.Data;
+using Admin2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Admin2.Areas.Identity.IdentityHostingStartup))]
+namespace Admin2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Admin2Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Admin2ContextConnection")));
+
+                services.AddDefaultIdentity<Admin2User>(options =>
+                {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                }).AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<Admin2Context>();
+            });
+        }
+    }
+}
